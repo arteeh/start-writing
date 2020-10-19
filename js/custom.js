286c9adfd992de75a8cb19078f1ed7466a18724a
Maarten de Jong <accounts@arteeh.com>
2020-10-19 15:49:25 +0200

    20 more prompts

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-// Background colors in alphabetical order, following this list:
+// List of background colors in alphabetical order, following this list:
 // https://www.w3schools.com/colors/colors_names.asp
 var colorList = [
 	"AliceBlue",
@@ -41,14 +41,32 @@ var colorList = [
 	"LightSkyBlue"	
 ];
 
-// Try to keep sentences around equal length using <br>&nbsp
+// Array of challenges to choose from
 var challengeList = [
 	"Write about something that made you happy this week.",
-	"Write about the most exciting thing that has happened to you in the last 30 minutes.",
-	"Write about the stupidest thing you've done."
+	"Write about the most exciting thing that has happened to you in the last hour.",
+	"Write about the stupidest thing you've done.",
+	"Write about what you think your life will look like in 5 years.",
+	"Write about a film or book that has stayed with you.",
+	"Write a letter to your 10-year-old self.",
+	"Write a letter to your future self.",
+	"Write about a memorable interaction with a stranger.",
+	"Write about what you'd do if money wasn't an issue.",
+	"Write about something you loved to do when you were a child.", //10
+	"Write about your responsibilities and how you feel about them.",
+	"Write about what keeps you going every day.",
+	"Write a thank you letter to yourself for the things you do.",
+	"Write about your ideal home.",
+	"Write about that one thing you want to do the most right now.",
+	"Look around you and pick an object. Write about why it's there.",
+	"Write down why you think your friends like you.",
+	"Who is your hero? Write down something about them that you admire.",
+	"Your TED Talk is tomorrow. Write down what you'll talk about and why.",
+	"Are you upset at someone? Write down why.", //20
+	"Who do you feel sorry for? Write down why.",
+	"Write about what in your life scares you the most.",
+	"Write."
 ];
-
-var maxLength = 150;
 
 // Set the color at launch
 setColor();
@@ -66,16 +84,6 @@ function setChallenge()
 {
 	// Get a challenge from the challenge list
 	var challenge = challengeList[Math.floor(Math.random() * challengeList.length)];
-	
-	/*
-	var thisLength = challenge.length;
-	var toAdd = maxLength - thisLength;
-	while(toAdd > 0)
-	{
-		challenge = challenge.concat(" &nbsp");
-		toAdd--;
-	}
-	*/
 	
 	// Fill the challenge with a challenge from the challenge list
 	document.getElementById("challenge").innerHTML = challenge;
